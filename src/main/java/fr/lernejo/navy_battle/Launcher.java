b5050653ce2504c60ef0cd1f4ab1235a1291654c
Tony Blard <tony.blard@epita.fr>
2022-03-10 11:32:49 +0100

    Part 2 Server HTTP

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,7 +1,35 @@
 package fr.lernejo.navy_battle;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Launcher {
     public static void main(String[] args) {
+        int port = Integer.parseInt(args[0]);
+        ExecutorService thread = Executors.newFixedThreadPool(1);
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+            server.setExecutor(thread);
+            System.out.println("Server start at port " + port);
+            server.createContext("/ping", Launcher::handle );
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+    }
+    private static void handle(HttpExchange exchange) throws IOException {
+        String body = "Hello";
+        exchange.sendResponseHeaders(200, body.length());
+        try (OutputStream os = exchange.getResponseBody()) { // (1)
+            os.write(body.getBytes());
+        }
     }
 }
