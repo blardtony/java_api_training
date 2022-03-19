0c7900e9c897481caa2b1d55595e2711decc9d7f
Tony Blard <tony.blard@epita.fr>
2022-03-19 12:16:45 +0100

    Rename Ping to PingHandler

--- src/main/java/fr/lernejo/navy_battle/PingHandler.java
+++ src/main/java/fr/lernejo/navy_battle/PingHandler.java
@@ -5,14 +5,15 @@ import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
 
-public class Ping implements HttpHandler {
+public class PingHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         String body = "OK";
-        exchange.sendResponseHeaders(200, body.length());
-        try (OutputStream os = exchange.getResponseBody()) { // (1)
+        exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, body.length());
+        try (OutputStream os = exchange.getResponseBody()) {
             os.write(body.getBytes());
         }
         exchange.close();
