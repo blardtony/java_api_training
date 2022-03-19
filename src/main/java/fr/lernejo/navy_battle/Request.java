ec475033336f031e68915146fa2cc913da56b301
Tony Blard <tony.blard@epita.fr>
2022-03-19 12:17:10 +0100

    Change record to Class

--- src/main/java/fr/lernejo/navy_battle/Request.java
+++ src/main/java/fr/lernejo/navy_battle/Request.java
@@ -1,9 +1,31 @@
 package fr.lernejo.navy_battle;
 
+public final class Request {
+    private final String id;
+    private final String url;
+    private final String message;
 
-record Request(
-    String id,
-    String url,
-    String message
-) {
+    public Request(String id, String url, String message) {
+        this.id = id;
+        this.url = url;
+        this.message = message;
+    }
+
+    public Request() {
+        this.id = "";
+        this.url = "";
+        this.message = "";
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
